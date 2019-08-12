6c729b1f6172959439800a90c79e5081db0ccd3c
jason.omemu <jasonjasyno50@gmail.com>
2019-08-12 11:33:05 +0100

    Updates

---
+++ precache-manifest.3c4cdc53e54559dae8b71a434a17b333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f27e1ac5f00d820e5f2d43910cf2d8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "08885aae1b9ac6df68cf",
+    "url": "/monsters-rolodex/static/css/main.6e459fe7.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "08885aae1b9ac6df68cf",
+    "url": "/monsters-rolodex/static/js/main.4dfebcdb.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
