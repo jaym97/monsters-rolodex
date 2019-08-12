a3eb6a4a5196974413bad7041f4150055de568e1
jason.omemu <jasonjasyno50@gmail.com>
2019-08-12 11:36:46 +0100

    initial file commit for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component.jsx';
+import {SearchBox} from './components/search-box/search-box.component.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFieldString: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
+  filterMonsterList = e => (this.setState({searchFieldString: e.target.value}))
+
+  render() {
+    const {monsters, searchFieldString} = this.state,
+          filteredMonsterList = monsters.filter(monster => 
+            monster.name.toLowerCase().includes(searchFieldString.toLowerCase())
+          );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.filterMonsterList}
+        />
+        <CardList monsters={filteredMonsterList} />
+      </div>
+    )
+  }
 }
 
 export default App;
